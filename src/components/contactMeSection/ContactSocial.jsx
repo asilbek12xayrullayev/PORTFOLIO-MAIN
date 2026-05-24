be1df0a942632c74edfad632aa2ea7116afc8a8d
asilbek12xayrullayev <ax039281@gmail.com>
2026-05-24 10:50:24 +0500

    update

---
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -0,0 +1,18 @@
+import SingleContactSocial from "./SingleContactSocial";
+import { FaLinkedinIn } from "react-icons/fa";
+// import { FiGithub } from "react-icons/fi";
+import { SiLeetcode } from "react-icons/si";
+// import Tooltip from '../ui/Tooltip.jsx'
+
+const ContactSocial = () => {
+  return (
+    <div className="flex gap-4">
+      {/* <SingleContactSocial link="https://www.linkedin.com/in/asilbek-xayrullayev-82b968405/" Icon={FaLinkedinIn} /> */}
+      {/* <SingleContactSocial link="https://github.com/asilbek12xayrullayev" Icon={FiGithub} /> */}
+      {/* <Tooltip /> */}
+      {/* <SingleContactSocial link="https://leetcode.com/u/utkxrsh13/" Icon={SiLeetcode} /> */}
+    </div>
+  );
+};
+
+export default ContactSocial;
